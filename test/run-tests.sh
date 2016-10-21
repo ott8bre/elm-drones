9e9f85b408e9480e669f920ab767564eeda7f7f5
Francesco Frosini <ott8bre@gmail.com>
2016-10-21 10:31:36 +0200

    updated run-test.sh

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -6,13 +6,13 @@ set -e
 
 elm-package install -y
 
-#VERSION_DIR="$(ls elm-stuff/packages/elm-lang/core/)"
-#CORE_PACKAGE_DIR="elm-stuff/packages/elm-lang/core/$VERSION_DIR"
-#CORE_GIT_DIR="$(dirname $PWD)"
+VERSION_DIR="$(ls elm-stuff/packages/elm-lang/core/)"
+CORE_PACKAGE_DIR="elm-stuff/packages/elm-lang/core/$VERSION_DIR"
+CORE_GIT_DIR="$(dirname $PWD)"
 
-#echo "Linking $CORE_PACKAGE_DIR to $CORE_GIT_DIR"
-#rm -rf $CORE_PACKAGE_DIR
-#ln -s $CORE_GIT_DIR $CORE_PACKAGE_DIR
+echo "Linking $CORE_PACKAGE_DIR to $CORE_GIT_DIR"
+rm -rf $CORE_PACKAGE_DIR
+ln -s $CORE_GIT_DIR $CORE_PACKAGE_DIR
 
 elm-make --yes --output test.js Test.elm
 node test.js
